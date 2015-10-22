0dc9d6c9910be8e22d64d87ed6e3d2eeef37d31b
Prakhar Gupta <mywolfdale@gmail.com>
2015-10-22 13:50:58 +0530

    Create Tweet_Media.py

---
+++ Tweet_Media.py
@@ -0,0 +1,17 @@
+import tweepy
+import os
+import sys
+ckey= ' '
+csecret= ' '
+atoken=' '
+asecret= ' ' 
+
+auth = tweepy.OAuthHandler(ckey,csecret) #Authenticating with twitter
+auth.set_access_token(atoken,asecret)    #Access tokens
+
+api = tweepy.API(auth) #Performing Auth
+filename = os.path.abspath(sys.argv[1])
+status = sys.argv[2]
+print "Posting Now"
+api.update_with_media(filename,status=status)
+
